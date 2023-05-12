93b81c20c3e180550e8db3b147ca9e2c58a5744f
tsurubaso <tsurubaso@gmail.com>
2023-05-12 19:58:38 +0900

    6.53 min toutes les pages vides sans les components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home</div>,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+]);
+
 function App() {
-  return <div>
-    Hello World!
-    </div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
