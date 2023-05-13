5687855b770e2c4c9a707bb03afee1089119a5e5
tsurubaso <tsurubaso@gmail.com>
2023-05-13 19:15:42 +0900

    34:35 finished Home css begginning jsx of Single page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+//import axios from "axios";
 
 const Home = () => {
   const posts = [
