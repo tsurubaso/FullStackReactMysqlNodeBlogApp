f51ee463469dd4f435bfcf30000e5567387fa686
tsurubaso <tsurubaso@gmail.com>
2023-05-14 09:55:23 +0900

    42 Menu est fini je commence write

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
 import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
@@ -26,7 +27,7 @@ const Single = () => {
 
       </p>
       </div>
-      <div className="menu">Menu</div>
+      <Menu />
     </div>
   );
 };
