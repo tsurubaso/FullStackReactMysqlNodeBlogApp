cd9df8d74c3f411a6b24d549201e7d3792865491
tsurubaso <tsurubaso@gmail.com>
2023-05-15 12:22:47 +0900

    1:03:35 first testing routes before controllers and routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ import express from "express"
 const app = express()
 
 app.use(express.json())
+
+
+
+app.get('/test/', function(req, res) {
+    res.json({data:"it works"})}) 
+    
 app.listen(8800,()=>{
     console.log("listening on 8800")
 })
