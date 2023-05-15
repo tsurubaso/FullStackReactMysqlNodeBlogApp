eda2396bdca77af67ed2cc48a83ddbfac6596475
tsurubaso <tsurubaso@gmail.com>
2023-05-15 17:37:35 +0900

    ok problem with the url, 404

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,12 @@
 import { db } from "../db.js";
+import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
   //check existing user
 
   const q = "SELECT * FROM users WHERE email=? OR username=?";
 
-  db.query(q, [req.body.email.req.body.name], (err, data) => {
+  db.query(q, [req.body.email, req.body.name], (err, data) => {
     if (err) return res.json(err);
     if (data.length) return res.status(409).json("User already exists!");
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+
+  const [inputs, setInputs]= useState({
+    username: "",
+    email: "",
+    password: "",
+
+  })
+  const [err, setError] = useState(null);
+
+
+  console.log(inputs) //tu efface après
+  const handleChange=e => {
+    setInputs(prev=>({...prev, [e.target.name]:e.target.value}))
+  }
+
+  const handleSubmit= async e => {
+    e.preventDefault()
+    try {
+      const res=await axios.post("auth/register", inputs)
+      console.log(res)
+    } catch (err) {
+      console.log(err.response.data);
+      
+    }
+    
+  }
+
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+        <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
+        <input required type="email" placeholder="email" name="email" onChange={handleChange}/>
+        <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
+        <button onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
         <span>Do you have an account? <Link to="/login">Login</Link></span>
       </form>
