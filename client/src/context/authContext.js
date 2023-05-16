728a65036e80e8dcdc3e5df6f8fba59a8803e68b
tsurubaso <tsurubaso@gmail.com>
2023-05-17 07:37:02 +0900

    some small schanges

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -16,23 +16,18 @@ export const AuthContextProvider = ({ children }) => {
     setCurrentUser(res.data);
   };
 
-
   const logout = async (inputs) => {
-    await axios.post(
-      "http://localhost:8800/api/auth/logout",
-      inputs
-    );
+    await axios.post("http://localhost:8800/api/auth/logout", inputs);
     setCurrentUser(null);
   };
 
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser)); //
+  }, [currentUser]);
 
-useEffect(() => {
-  localStorage.setItem("user", JSON.stringify(currentUser)); //
-},[currentUser]);
-
-return  <AuthContext.Provider value={{currentUser, login, logout}}>{children}</AuthContext.Provider>
-  
-
-
-
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
