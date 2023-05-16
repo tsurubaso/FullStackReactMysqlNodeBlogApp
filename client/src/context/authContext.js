2d2e234a18a4c6007d291fae54fe959ae09c2a56
tsurubaso <tsurubaso@gmail.com>
2023-05-16 16:14:37 +0900

    un peu avancé je comprends rien

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -3,5 +3,16 @@ import {createContext} from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) = {
+    const [currentUser.setCurrentUser]=useState(JSON.parse(localStorage.getItem('user')|| null)
     
+    const login = async(inputs)=>{
+
+      const  res = await axios.post(
+            "http://localhost:8800/api/auth/login",
+            inputs
+          );
+    }
+    
+    
+    );
 }
